15d624594bbd49e0d313314da409a3570060147a
Oksana Tonkopriad <tonkopryad84@gmail.com>
2022-09-12 15:19:06 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
+// Описан в документации
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsEl = document.querySelector(`.gallery`);
+const imgGallery = createGalleryItems(galleryItems);
+
+galleryItemsEl.insertAdjacentHTML("beforeEnd", imgGallery);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  nav: true,
+  close: true,
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
